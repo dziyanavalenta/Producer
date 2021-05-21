acd8b7223efdefcc583ec4412ab8360b7e2af8d5
d.valenta <dziyana.valenta@bglgroup.co.uk>
2021-05-21 14:38:02 +0300

    Get request Hi

--- src/main/java/dvalenta/Producer/Controllers/ClientController.java
+++ src/main/java/dvalenta/Producer/Controllers/ClientController.java
@@ -1,4 +1,13 @@
 package dvalenta.Producer.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ClientController {
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hi";
+    }
+
 }
