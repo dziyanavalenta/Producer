d7440a1f49205d93e1c92a1d336bebdae1eb3880
d.valenta <dziyana.valenta@bglgroup.co.uk>
2021-05-22 06:13:35 +0300

    Added /kafka to url

--- src/main/java/dvalenta/Producer/Controllers/ClientController.java
+++ src/main/java/dvalenta/Producer/Controllers/ClientController.java
@@ -3,16 +3,18 @@ package dvalenta.Producer.Controllers;
 import dvalenta.Producer.Models.Client;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kafka")
 
 public class ClientController {
 
     @PostMapping("/clients")
     public String addClient(@RequestBody Client newClient) {
         System.out.println("This is id: " + newClient.getClientId());
-        return "new success";
+        return "new success with kafka url";
     }
 }
 
